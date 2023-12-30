4daac9dd3b0aa1fd8572db3f985ce993c6f460bb
Mercadeo <publicidadfrescas@gmail.com>
2023-12-30 10:10:37 -0500

    subida

---
+++ README.md
@@ -0,0 +1,7 @@
+# Formulario Frescas & Maduradas
+
+
+Envía información requerida del formulario a un correo electrónico
+Implementación de Api Resend para el envío de información
+Implementación php
+Instancia de Resend con composer
---
+++ Resend.php
@@ -0,0 +1,131 @@
+<?php
+// Carga la biblioteca Resend y Configura limites del tamaño de carga de archivos
+require __DIR__ . '/vendor/autoload.php';
+ini_set('File_uploads', 'On')
+ini_set('upload_max_filesize', '20M');
+ini_set('post_max_size', '20M');
+
+try {
+    //Iniciar la instancia de Resend
+    $resend = Resend::client('re_ZKbLvu5d_BU4FubgbBDvzgGUqMx89JzbR');
+
+} catch (Exception $e) {
+    // Manejar el error si no se puede crear la instancia de Resend 
+    echo "Error al inicializar Resend: " . $e->getMessage();
+    exit();
+}
+
+// Verifica si el formulario se envía con el método post
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Obtener y limpiar los datos del formulario
+    $nombre = htmlspecialchars($_POST["Name"]);
+    $email = filter_var($_POST["Email"], FILTER_SANITIZE_EMAIL);
+    $vacante = htmlspecialchars($_POST["Vacante"]);
+    $experiencia = htmlspecialchars($_POST["Experiencia"]);
+    $telefono = htmlspecialchars($_POST["Teléfono"]);
+    $seleccion = htmlspecialchars($_POST["Selección"]);
+
+    // Agregar archivo adjunto y verifica si hay algún archivo adjunto
+    if (isset($_FILES["file"]) && $_FILES["file"]["error"] == UPLOAD_ERR_OK) {
+        
+        // Directorio donde se almacenarán los archivos subidos
+        $directorio_destino = "archivos/";
+        
+        // Obtener información sobre el archivo
+        $nombre_archivo = basename($_FILES["file"]["name"]);
+        $ruta_archivo = $directorio_destino . $nombre_archivo;
+
+        // Mover el archivo al directorio de destino
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $ruta_archivo)) {
+
+        //Verifica si el archivo existe
+        if (!file_exists($ruta_archivo)) {
+            echo "El archivo no existe: $ruta_archivo ";
+            exit();
+        }
+            switch ($_FILES["file"]["error"]) {
+                case UPLOAD_ERR_OK:
+                    // Todo bien
+                    break;
+                case UPLOAD_ERR_INI_SIZE:
+                case UPLOAD_ERR_FORM_SIZE:
+                    echo "El archivo es demasiado grande.";
+                    break;
+                case UPLOAD_ERR_PARTIAL:
+                    echo "El archivo se subió parcialmente.";
+                    break;
+                case UPLOAD_ERR_NO_FILE:
+                    echo "No se seleccionó ningún archivo.";
+                    break;
+                case UPLOAD_ERR_NO_TMP_DIR:
+                    echo "Falta el directorio temporal.";
+                    break;
+                case UPLOAD_ERR_CANT_WRITE:
+                    echo "Error al escribir el archivo en el servidor.";
+                    break;
+                case UPLOAD_ERR_EXTENSION:
+                    echo "Error de extensión del archivo.";
+                    break;
+                    exit();
+            }
+        } else {
+            echo "Error al subir el archivo.";
+            exit();
+        }
+
+    } else {
+        echo "No se ha seleccionado ningún archivo o se produjo un error en la carga.";
+        exit();
+    }
+
+    // Construir el cuerpo del correo
+    $cuerpo = "<p> Nombre:  $nombre\r\n </p>";
+    $cuerpo .= "<p> Correo: $email\r\n </p>";
+    $cuerpo .= "<p> Vacante: $vacante\r\n </p>";
+    $cuerpo .= "<p> Experiencia: $experiencia\r\n </p>";
+    $cuerpo .= "<p> Teléfono: $telefono\r\n </p>";
+    $cuerpo .= "<p> Selección: $seleccion\r\n </p>";
+
+    // Intentar enviar el correo electrónico con Resend
+    try {
+        $resend->emails->send([
+            'from' => 'Formulario <onboarding@resend.dev>',
+            'to' => ['publicidadfrescas@gmail.com'],
+            'subject' => 'Hoja de vida',
+            'html' => "$cuerpo",
+
+            'attachments' => [
+                [
+                    'filename' => "$nombre_archivo",
+                    'content' =>  $ruta_archivo
+                ]
+            ],
+
+            'headers' => [
+                'X-Entity-Ref-ID' => 're_ZKbLvu5d_BU4FubgbBDvzgGUqMx89JzbR',
+            ],
+            'tags' => [
+                [
+                    'name' => 'category',
+                    'value' => 'confirm_email',
+                ],
+            ],
+        ]);
+
+        // Redirigir a una página de agradecimiento
+        header("Location: index.html");
+        exit();
+
+    } catch (Exception $e) {
+        
+        // Manejar el error si falla el envío del correo electrónico con Resend
+        echo "Error al enviar el correo electrónico con Resend: " . $e->getMessage();
+        exit();
+    }
+} else {
+
+    // Código si no hay datos en $_POST
+    echo "Error: No se recibieron datos del formulario.";
+}   
+?>  
